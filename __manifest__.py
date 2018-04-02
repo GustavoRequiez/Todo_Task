c76211f6ce541200d75f5174db23147fbd5da8a9
ADMONERP\GFLORES <gflores@requiez.com>
2018-04-02 17:47:46 -0500

    Modulo Odoo

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal
+    To-Do
+    tasks.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'Data': ['views/todo_menu.xml'],
+    'application': True,
+}
